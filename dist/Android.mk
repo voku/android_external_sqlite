e953bce8a1ee3ae6285627d87f180bfc4a5c4f6e
Lars Moelleken <voku@voku-online.de>
2012-09-06 14:13:28 +0200

    + testing ...

--- dist/Android.mk
+++ dist/Android.mk
@@ -28,7 +28,11 @@ common_sqlite_flags := \
 	-DSQLITE_OMIT_COMPILEOPTION_DIAGS \
 	-DSQLITE_OMIT_LOAD_EXTENSION \
 	-DSQLITE_DEFAULT_FILE_PERMISSIONS=0600 \
-	-Dfdatasync=fdatasync
+	-Dfdatasync=fdatasync \
+	-march=armv7-a \
+	-mfloat-abi=softfp \
+	-mfpu=neon \
+	-O3
 
 common_src_files := sqlite3.c
 
